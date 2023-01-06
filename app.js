88550ecd181a35cfa5f6e29eee46c9e28fb76469
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-06 14:48:06 -0800

    added wireframe

---
+++ app.js
@@ -0,0 +1,35 @@
+/* Imports */
+
+/*const { blanchedalmond } = require("color-name")*/
+
+/* Get DOM Elements */
+
+/* State */
+
+/* Events */
+
+/*button clicks for prompt and ask again
+hiding 8 ball and random answer
+show prompt
+hide prompt
+show image
+pick random number in Array
+set p to random answer */
+
+/* Display Functions */
+
+// (don't forget to call any display functions you want to run on page load!)
+
+/* HTML */
+/* <!-- h1 Header
+ Section
+    p tag w/ prompt
+    input Bar
+    submit btn 
+Section 
+
+section
+    image of ball 
+    p tag with random answer
+    ask again button
+section */
