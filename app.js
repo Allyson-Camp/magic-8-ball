55fe130f9afd5c97a52758487478685622a50a94
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-06 16:01:06 -0800

    added HTML sections

--- app.js
+++ app.js
@@ -4,9 +4,56 @@
 
 /* Get DOM Elements */
 
+const submitButton = document.getElementById('submit');
+const promptPage = document.getElementById('prompt');
+const outcomePage = document.getElementById('outcome');
+const answerP = document.getElementById('answer');
+const resetButton = document.getElementById('reset');
 /* State */
 
 /* Events */
+
+submitButton.addEventListener('click', () => {
+    toggleSections();
+    const randomInt = Math.floor(Math.random() * answers.length);
+    const randomChoice = answers[randomInt];
+    console.log(randomInt);
+    console.log(randomChoice);
+    console.log('answers[12]'), answers[12];
+    answerP.textContent = randomChoice;
+});
+
+resetBtn.addEventListener('click', () => {
+    // hide 8 ball and the answer
+    // show prompt
+});
+
+function toggleSections() {
+    promptPage.classList.toggle('hide');
+    outcomePage.classList.toggle('hide');
+}
+
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Outlook hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
 /*button clicks for prompt and ask again
 hiding 8 ball and random answer
